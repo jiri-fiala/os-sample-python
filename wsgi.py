36b6bbcfa499b1edc61b1c5c28cbd195666f6086
Jiří Fiala <jfiala@redhat.com>
2016-10-11 10:55:06 +0200

    Simple websocket test added, inspired by: https://github.com/miguelgrinberg/Flask-SocketIO
    gunicorn configured to use eventlet workers

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit, disconnect
+
+async_mode = "eventlet"
+
 application = Flask(__name__)
+application.config['SECRET_KEY'] = 'fsdjno342rfh094tn'
+socketio = SocketIO(application, async_mpde=async_mode)
 
 @application.route("/")
 def hello():
     return "Hello World!"
 
+@application.route("/ws")
+def wsecho():
+    return render_template("ws.html", async_mode=socketio.async_mode)
+
+@socketio.on('my_event', namespace='/test')
+def test_message(message):
+    emit('my_response', {'data': message['data']})
+
+@socketio.on('my_broadcast_event', namespace='/test')
+def test_message(message):
+    emit('my_response', {'data': message['data']}, broadcast=True)
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my_response', {'data': 'Connected'})
+
+@socketio.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    emit('my_response',  {'data': 'Disconnected!'})
+    disconnect()
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
 if __name__ == "__main__":
-    application.run()
+    #application.run()
+    socketio.run(application)
